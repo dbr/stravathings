2208aab935bb7eca062f00fb927979d8326bd9b7
dbr <dbr.onix@gmail.com>
2012-06-21 20:49:13 +0100

    Fix curve simplification

--- curve_simplify.py
+++ curve_simplify.py
@@ -10,7 +10,7 @@ def _vec2d_mult(p1, p2):
     return p1[0]*p2[0] + p1[1]*p2[1]
 
 
-def _ramerdouglas(line, dist):
+def ramerdouglas(line, dist):
     """Does Ramer-Douglas-Peucker simplification of a line with `dist`
     threshold.
 
@@ -27,52 +27,45 @@ def _ramerdouglas(line, dist):
 
     begin, end = line[0], line[-1]
 
-    if begin == end:
-        # Line segment begins/ends at same point
-        # FIXME: This potentially drops a lot of points
-        return [begin]
+    distSq = []
+    for curr in line[1:-1]:
+        tmp = (
+            _vec2d_dist(begin, curr) - _vec2d_mult(_vec2d_sub(end, begin), _vec2d_sub(curr, begin)) ** 2 / _vec2d_dist(begin, end))
+        distSq.append(tmp)
 
-    distSq = (
-        _vec2d_dist(begin, curr) - _vec2d_mult(_vec2d_sub(end, begin), _vec2d_sub(curr, begin)) ** 2 / _vec2d_dist(begin, end)
-        for curr in line[1:-1])
+    maxdist = max(distSq)
+    if maxdist < dist ** 2:
+        return [begin, end]
 
-    pos, maxdist = max(enumerate(distSq), key = lambda k: k[1])
-
-    return (_ramerdouglas(line[:pos + 2], dist) +
-            _ramerdouglas(line[pos + 1:], dist)[1:])
-
-
-def ramerdouglas(line, dist):
-    if line[0] != line[-1]:
-        return _ramerdouglas(line, dist)
-    else:
-        return _ramerdouglas(line[:-1], dist) + line[-1:]
+    pos = distSq.index(maxdist)
+    return (ramerdouglas(line[:pos + 2], dist) +
+            ramerdouglas(line[pos + 1:], dist)[1:])
 
 
 if __name__ == '__main__':
     def plot(pl):
         max_x = max(pl, key = lambda k: k[0])[0]
         max_y = max(pl, key = lambda k: k[1])[1]
-    
+
         min_x = min(pl, key = lambda k: k[0])[0]
         min_y = min(pl, key = lambda k: k[1])[1]
-    
+
         width, height = 16, 16
-    
+
         out_grid = [[" " for _ in range(width)] for _ in range(height)]
-    
+
         for p in pl:
             cur_x = p[0]
             cur_y = p[1]
             norm_x = (cur_x - min_x) / (max_x-min_x)
             norm_y = (cur_y - min_y) / (max_y-min_y)
-    
+
             out_grid[int(norm_x * (width-1))][int(norm_y*(height-1))] = "*"
-    
+
         for row in out_grid:
             print " ".join(c for c in row)
 
-    
+
     pl = [
         (0.0, 0.0),
         (1.0, 0.0),
@@ -92,7 +85,7 @@ if __name__ == '__main__':
     end = time.time()
 
     print "%.02fms" % ((end-start)*1000)
-    print len(simple)
+    print "%s reduced to %s" % (len(pl), len(simple))
 
     plot(pl)
     print "#"*40
